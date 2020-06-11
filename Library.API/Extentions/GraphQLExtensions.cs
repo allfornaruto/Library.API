039b1d027a449a7639914db2996ae42c5e894f08
BaoGuoQiang <827101896@qq.com>
2020-06-12 00:09:26 +0800

    .

---
+++ Library.API/Extentions/GraphQLExtensions.cs
@@ -0,0 +1,24 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using Library.API.GraphQLSchema;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Extentions
+{
+    public static class GraphQLExtensions
+    {
+        public static void AddGraphQLSchemaAndTypes(this IServiceCollection services)
+        {
+            services.AddSingleton<AuthorType>();
+            services.AddSingleton<BookType>();
+            services.AddSingleton<LibraryQuery>();
+            services.AddSingleton<ISchema, LibrarySchema>();
+            services.AddSingleton<IDocumentExecuter, DocumentExecuter>();
+            services.AddSingleton<IDependencyResolver>(provider => new FuncDependencyResolver(type => provider.GetRequiredService(type)));
+        }
+    }
+}
---
+++ Library.API/GraphQLSchema/LibrarySchema.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.GraphQLSchema
+{
+    public class LibrarySchema: Schema
+    {
+        public LibrarySchema(LibraryQuery query, IDependencyResolver dependencyResolver)
+        {
+            Query = query;
+            DependencyResolver = dependencyResolver;
+        }
+    }
+}
